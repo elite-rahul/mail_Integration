595ff9a331f3b0c3049e03497ff98bf2bbb55d56
Rahul <sikroharrahul@gmail.com>
2023-07-03 18:34:25 +0530

    Mail

---
+++ src/main/java/com/geekster/CreatingObjectsMailIntegration/CreatingObjectsMailIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.geekster.CreatingObjectsMailIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatingObjectsMailIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatingObjectsMailIntegrationApplication.class, args);
+	}
+
+}
